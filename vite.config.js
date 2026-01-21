2e82eddd6b42ed7802c788c1600c97c9446477bb
Laura Delissen <lauradelissen@hotmail.com>
2026-01-21 17:01:52 +0100

    vite aangepast naar '/'

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Zet hier de NAAM van jouw GitHub repository!
-const basePath = '/Portfolio/';
-
 export default defineConfig({
-  plugins: [tailwindcss(), react() ],
-  base: basePath,
+  plugins: [tailwindcss(), react()],
+  base: '/', // ← DIT is de fix
   build: {
     rollupOptions: {
       input: {
